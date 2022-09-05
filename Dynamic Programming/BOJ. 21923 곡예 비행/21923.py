77eb662633fa8eba36434690bc8f6f508289e538
SungOh98 <tryjso@naver.com>
2022-09-05 23:10:58 +0900

    2022/09/05

--- Dynamic Programming/BOJ. 21923 곡예 비행/21923.py
+++ Dynamic Programming/BOJ. 21923 곡예 비행/21923.py
@@ -14,8 +14,8 @@ DP_UP[i][j]
 1. DP 테이블 정의
 DP[i][j] : 시작 점으로부터 board[i][j] 까지 왔을 때 최대 점수
 2. 점화식
-DP_UP[i][j] = max(DP_UP[i + 1][j], DP_UP[i][j - 1]) + board[i][j]
-DP_DOWN[i][j] = max(DP_DOWN[i + 1][j], DP_DOWN[i][j + 1]), board[i][j]
+DP_UP[i][j] += max(DP_UP[i + 1][j], DP_UP[i][j - 1])
+DP_DOWN[i][j] += max(DP_DOWN[i + 1][j], DP_DOWN[i][j + 1])
 
 DP[i][j] = DP_UP[i][j] + DP_DOWN[i][j]
 
@@ -24,10 +24,12 @@ DP[i][j] = DP_UP[i][j] + DP_DOWN[i][j]
 
 def take_off(dp):
     # 초기설정 >> 이것때문에 낮설음
+    # 출발점을 포함한 행/ 열은 출발점으로부터 따로 갱신
     for i in range(1, m):
         dp[n - 1][i] += dp[n - 1][i - 1]
     for i in range(n - 2, -1, -1):
         dp[i][0] += dp[i + 1][0]
+    # 점화식 도출
     for i in range(n - 2, -1, -1):
         for j in range(1, m):
             dp[i][j] += max(dp[i + 1][j], dp[i][j - 1])
@@ -36,16 +38,18 @@ def take_off(dp):
 
 def land(dp):
     # 초기 설정
+    # 출발점을 포함한 행/ 열은 출발점으로부터 따로 갱신
     for i in range(m - 2, -1, -1):
         dp[n - 1][i] += dp[n - 1][i + 1]
     for i in range(n - 2, -1, -1):
         dp[i][m - 1] += dp[i + 1][m - 1]
-
+    # 점화식 도출
     for i in range(n - 2, -1, -1):
         for j in range(m - 2, -1, -1):
             dp[i][j] += max(dp[i][j + 1], dp[i + 1][j])
     # for i in dp:
     #     print(i)
+# 두 DP테이블의 요소들을 합한 각 요소중 최대값이 정답.
 def answer(dp1, dp2):
     max_v = -float('inf')
     for i in range(n):
